42d6cfa2a3d464ff5b9525ed06014d53a59224c6
Marcio Ciolfi <ciolfi@gmail.com>
2019-04-07 23:50:14 -0300

    Update Multiple_linear_regression.py

--- SAE/Brake Pipes/Multiple_linear_regression.py
+++ SAE/Brake Pipes/Multiple_linear_regression.py
@@ -15,34 +15,33 @@ __status__ = 'Production'
 def arq(name, ndata, types):
     import csv
 
-    listt = []  # Define listt as matrix
-    with open(name, newline='') as csvfile:  # csv module will detect new lines
-        if types == ' ':  text = csv.reader(csvfile, delimiter=' ')  # classify by space
-        if types == ',':  text = csv.reader(csvfile, delimiter=',')  # classify by comma
-        if types == '\t': text = csv.reader(csvfile, delimiter='\t')  # classify by tab
+    listt = []                                                          # Define listt as matrix
+    with open(name, newline='') as csvfile:                             # csv module will detect new lines
+        if types == ' ':  text = csv.reader(csvfile, delimiter=' ')     # classify by space
+        if types == ',':  text = csv.reader(csvfile, delimiter=',')     # classify by comma
+        if types == '\t': text = csv.reader(csvfile, delimiter='\t')    # classify by tab
         for line in text:
-            for t in range(len(line) - ndata): line.remove('')  # Removes zeros inside data
-            listt.append(line)  # Define listt as the data inside file
+            for t in range(len(line) - ndata): line.remove('')          # Removes zeros inside data
+            listt.append(line)                                          # Define listt as the data inside file
     return (listt)
 
 
 # Main program
 from numpy import exp, array, random, dot, square, log
 from numpy.linalg import inv
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
-listt = array(arq('pressures_nonzeros.dat', 3, '\t'))  # Define listt as the data inside the file
+# Define listt as the data inside the file
+listt = array(arq('pressures_nonzeros.dat', 3, '\t'))
 # Set input variables one, x1, x2, and output y as matrix and vector
 one = []
 x1 = []
 x2 = []
 y = []
-# plt.xlabel('pressure')
-# plt.ylabel('length')
-# plt.semilogy()
-for count in range(len(listt)):  # Convert the data string in values
+
+# Convert the data string in values
+for count in range(len(listt)):
     x1.append(float(listt[count][0]))
     x2.append(float(listt[count][1]))
     y.append([int(listt[count][2])])
@@ -57,11 +56,13 @@ for count in range(len(listt)):  # Convert the data string in values
     plt.grid(True)
     plt.xlabel ('External Diameter [mm]')
     plt.plot(x2, y, 'r.', label = 'External diameter')
+
 # Define x value and beta and turn variables as array
 y = array(y)
-x = [one, x1, log(x1), x2, square(x2)]  # Main function
-x = array(x).T  # Adaptating the matrix direction
-beta = dot(inv(dot(x.T, x)), dot(x.T, y))  # Finding beta coefficients
+x = [one, x1, log(x1), x2, square(x2)]      # Main function
+x = array(x).T                              # Adaptating the matrix direction
+beta = dot(inv(dot(x.T, x)), dot(x.T, y))   # Finding beta coefficients
+
 # Print program data
 print('# ' + '=' * 78)
 print('Author: ' + __author__)
@@ -76,10 +77,13 @@ print('Date: ' + __date__)
 print('Username: ' + __username__)
 print('Description: ' + __description__)
 print('# ' + '=' * 78)
+print()
 
 # Print main function
 print('beta = ',beta.T)
 print('f(pipe) =', int(beta[0]), '+', int(beta[1]), '* length +', int(beta[2]), '* ln(length) +', int(beta[3]), '* diameter +', int(beta[4]), '* diameter^2')
+
+
 # Testing the results of beta with sample
 for length in [1.5, 3, 19]:
     for dia in [4.5, 4.36, 4.15]:
